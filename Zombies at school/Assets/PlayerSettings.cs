1be8c73aecfc6ac2319bff7b3e88cf8d32d36876
Roman <barbarista1862@gmail.com>
2022-01-08 21:23:28 +0300

    my first commit
    start of game development

---
+++ Zombies at school/Assets/PlayerSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSettings : MonoBehaviour
+{
+    public float Speed;
+    private Vector3 _direction;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        _direction.x = Input.GetAxis("Horizontal");
+        _direction.z = Input.GetAxis("Vertical");
+        Debug.Log(_direction);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = transform.position + _direction * Time.fixedDeltaTime * Speed;
+    }
+}
